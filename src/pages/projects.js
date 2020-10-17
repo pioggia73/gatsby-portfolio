6495dd3406e26fe75b6725405fbb33725a702c1b
Monika Wasilewska <monikawasilewska13@wp.pl>
2020-10-17 20:54:10 +0200

    changes

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -31,7 +31,7 @@ export const query = graphql`
         image {
           childImageSharp {
             fluid {
-              ...ChildImageSharpFluid
+              ...childImageSharpFluid
             }
           }
         }
